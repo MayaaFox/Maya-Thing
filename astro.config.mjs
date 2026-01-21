3da2d726a8ce730df81b68079f99ec43772486c6
IfTheMatchSticks <ifthematchsticks@notreal.com>
2026-01-21 02:22:41 -0500

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://token-template.deno.dev",
+  site: "https://mayaafox.otherworldly.live",
   integrations: [mdx(), sitemap(), tailwind()],
+
   markdown: {
     shikiConfig: {
       theme: "github-dark-high-contrast",
